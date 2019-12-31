2aa4aa16c70503c1f2d3cf467cb1b19f947f4f6c
Angel LaVoie <35380720+K3io5u@users.noreply.github.com>
2019-12-31 06:14:22 -0500

    added new event

--- social.py
+++ social.py
@@ -10,12 +10,14 @@ class Social(commands.Cog):
     def __init__(self, bot):
         self.bot=bot
 
+
     @commands.command(name='invite', help='Invite Link for the Bot')
     async def server_invite(self, ctx):
         response='https://discordapp.com/api/oauth2/authorize?client_id=660646451273007127&permissions=8&scope=bot'
         await ctx.send('{} A Direct Message has Been Sent You.'.format(ctx.author.mention))
         await ctx.author.send('Here is the link to invite Handsome JackBot to your Discord.')
         await ctx.author.send(response)
+
 
     @commands.command(name='github', help='Github Repository for the Bot')
     async def github_link(self, ctx):
@@ -24,10 +26,25 @@ class Social(commands.Cog):
         await ctx.author.send('Here is the link to the Github Repo.')
         await ctx.author.send(response)
 
+
     @commands.command(name='randy', help='Randy Emote')
     async def bl_randy(self, ctx):
         response='<:justintime:646748813981384746>'
         await ctx.send(response)
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        guild=self.bot.get_guild(632633098584064018)
+
+        if message.author==self.bot.user:
+            return
+        
+        if message.guild==guild:
+            if 'randy' in message.content:
+                response='<:justintime:646748813981384746>'
+                await message.channel.send(response)
+
 
 def setup(bot):
     bot.add_cog(Social(bot))
