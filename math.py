1f0b73baf8f5824a3003faa2697eb9a61b0369ba
Angel LaVoie <35380720+K3io5u@users.noreply.github.com>
2020-01-08 01:38:15 -0500

    added math.py

---
+++ math.py
@@ -0,0 +1,31 @@
+# math.py
+
+# separate command for each vh, parse link to make sure it is the right vh and take the numbers into an array to assign values to each variable in the formula
+# need error defs for each def to catch missingargumentexceptions
+
+import discord
+from discord.ext import commands
+
+'''class Math(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot=bot
+
+    
+    #@commands.command(name='calcamara', help='Provide a BL3Skills link and let the Bot calc your Amara damage!')
+    #async def bl_calcamara(self, ctx, link):
+    
+
+    #@commands.command(name='calcflak', help='Provide a BL3Skills link and let the Bot calc your FL4K damage!')
+    #async def bl_calcflak(self, ctx, link):
+
+    
+    #@commands.command(name='calcmoze', help='Provide a BL3Skills link and let the Bot calc your Moze damage!')
+    #async def bl_calcmoze(self, ctx, link):
+
+
+    #@commands.command(name='calczane', help='Provide a BL3Skills link and let the Bot calc your Zane damage!')
+    #async def bl_calczane(self, ctx, link):
+
+    def setup(bot):
+        bot.add_cog(Math(bot))'''
